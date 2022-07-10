2e88bfbee2995a9e6c29b2d3582ae797f3afbb26
Badi-Haki Martin <jojackblack@gmail.com>
2022-07-10 17:16:57 -0400

    commit tree 2660dedb7733c75708c45086e360513def20309e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self[-1] == "."
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(" ")
+    #binding.pry
+    sentences.filter do |word|
+     word.end_with?(".") || word.end_with?("?") || word.end_with?("!")
+    end.count
   end
+
 end
